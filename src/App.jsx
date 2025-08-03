adf3eb176d6e039ff4e9b6c74b0d825db7e40a7a
pooja Kumari <pdpoojakumari@gmail.com>
2025-08-03 22:09:55 +0530

    fixed design

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/thank-you" element={<ThankYouPage />} />
-        
       </Routes>
     </BrowserRouter>
     </>
